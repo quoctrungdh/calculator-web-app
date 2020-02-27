37cecb7ad46ae430f1755dd461d8856022164f6e
TrungDo <trungdo@TrungDos-MacBook-Pro.local>
2020-02-28 00:36:06 +0700

    change operator text color from white to black to improve visible ability

--- src/utils/configs.ts
+++ src/utils/configs.ts
@@ -13,18 +13,18 @@ export const keysConfig = [
     { value: 7, event: CalculatorEvents.setExpression },
     { value: 8, event: CalculatorEvents.setExpression },
     { value: 9, event: CalculatorEvents.setExpression },
-    { value: Operators.devide, event: CalculatorEvents.setOperator, style: "bg-orange-500" },
+    { value: Operators.devide, event: CalculatorEvents.setOperator, style: "bg-orange-500 text-black" },
     { value: 4, event: CalculatorEvents.setExpression },
     { value: 5, event: CalculatorEvents.setExpression },
     { value: 6, event: CalculatorEvents.setExpression },
-    { value: Operators.multiply, event: CalculatorEvents.setOperator, style: "bg-orange-500" },
+    { value: Operators.multiply, event: CalculatorEvents.setOperator, style: "bg-orange-500 text-black" },
     { value: 1, event: CalculatorEvents.setExpression },
     { value: 2, event: CalculatorEvents.setExpression },
     { value: 3, event: CalculatorEvents.setExpression },
-    { value: Operators.substract, event: CalculatorEvents.setOperator, style: "bg-orange-500" },
+    { value: Operators.substract, event: CalculatorEvents.setOperator, style: "bg-orange-500 text-black" },
     { value: Operators.clear, event: CalculatorEvents.clear, style: "bg-gray-200 text-black" },
     { value: 0, event: CalculatorEvents.setExpression },
     { value: Operators.equal, event: CalculatorEvents.evaluateExpression },
-    { value: Operators.add, event: CalculatorEvents.setOperator, style: "bg-orange-500" }
+    { value: Operators.add, event: CalculatorEvents.setOperator, style: "bg-orange-500 text-black" }
 ]
 
